a99053fe8a6f110b7b9acba036f9b6b37908876b
sayanidey29 <sayanidey199829@gmail.com>
2024-06-08 01:09:18 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
-//Auth API Route
+import jwt from "jsonwebtoken";
+//Auth API Route for SignUp
 export const signup = async (req, res, next) => {
   console.log("request_body:", req.body);
   // console.log("request:", req);
@@ -35,6 +36,38 @@ export const signup = async (req, res, next) => {
   } catch (error) {
     // console.log("catch", error);
     // return res.status(500).json({ message: error.message });
+    next(error);
+  }
+};
+
+//Auth API Route for SignIn
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(400, "All fields are required"));
+  }
+  try {
+    const validUser = await User.findOne({ email });
+    console.log("validUser", validUser);
+    if (!validUser) {
+      return next(errorHandler(400, "User not found")); //write the return statement so it do not go to next line after error
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser?.password);
+    console.log("validPassword", validPassword);
+    if (!validPassword) {
+      return next(errorHandler(400, "Invalid Password")); //write the return statement so it do not go to next line after error
+    }
+    const token = jwt.sign({ id: validUser?._id }, process.env.JWT_SECRET);
+    console.log("token", token);
+    console.log("validUser._doc", validUser?._doc);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
+  } catch (error) {
     next(error);
   }
 };
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { signup } from "../controllers/auth.controller.js";
+import { signin, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-//Auth API Route
+//Auth API Route for SignUp
 router.post("/signup", signup);
+//Auth API Route for SignIn
+router.post("/signin", signin);
 
 export default router;
