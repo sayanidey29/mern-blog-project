18c544f25d55fd4da2ca3b3a1ca947f89aef1228
sayanidey29 <sayanidey199829@gmail.com>
2024-06-15 23:59:42 +0530

    Added user Image upload functionality

--- clientmarnblog/src/components/DashProfile.jsx
+++ clientmarnblog/src/components/DashProfile.jsx
@@ -1,20 +1,139 @@
-import { Button, TextInput } from "flowbite-react";
-import React from "react";
+import { Alert, Button, TextInput } from "flowbite-react";
+import React, { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 const DashProfile = () => {
   const { currentUser } = useSelector((state) => state.user);
+  const [imageFile, setImageFile] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const filePickerRef = useRef();
+  const [imageFileUploadingProgress, setImageFileUploadingProgress] =
+    useState(null);
+  const [imageFileUploadError, setImageFileUploadError] = useState(null);
+  console.log("imageFileUploadingProgress", imageFileUploadingProgress);
+  console.log("imageFileUploadError", imageFileUploadError);
+
+  const handleImageChange = (e) => {
+    console.log("event", e);
+    const file = e.target.files[0];
+    if (file) {
+      setImageFile(file);
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+    console.log("imageFile", imageFile);
+    console.log("imageFileUrl", imageFileUrl);
+  };
+
+  useEffect(() => {
+    if (imageFile) {
+      uploadImage();
+      console.log("imageFile", imageFile);
+      console.log("imageFileUrl", imageFileUrl);
+    }
+  }, [imageFile]);
+  const uploadImage = async () => {
+    //---FIREBASE RULES---//
+    // service firebase.storage {
+    //     match /b/{bucket}/o {
+    //       match /{allPaths=**} {
+    //         allow read;
+    //         allow write: if
+    //         request.resource.size< 2*1024*1024 &&
+    //         request.resource.contentType.matches('image/.*')
+    //       }
+    //     }
+    //   }
+    setImageFileUploadError(null);
+    console.log("uploading image...");
+    const storage = getStorage(app);
+    console.log("storage", storage);
+    const fileName = new Date().getTime() + "_" + imageFile.name;
+    console.log("fileName", fileName);
+    const storageRef = ref(storage, fileName);
+    console.log("storageRef", storageRef);
+    const uploadTask = uploadBytesResumable(storageRef, imageFile);
+    console.log("uploadTask", uploadTask);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("progress", progress);
+        setImageFileUploadingProgress(progress.toFixed(0));
+      },
+      (error) => {
+        setImageFileUploadError(
+          "Could not upload image(File must be less than 2MB)"
+        );
+        setImageFileUploadingProgress(null);
+        setImageFile(null);
+        setImageFileUrl(null);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          console.log("downloadURL", downloadURL);
+          setImageFileUrl(downloadURL);
+        });
+      }
+    );
+  };
   return (
     <div className="max-w-lg mx-auto p-3 w-full font-semibold">
       <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
       <form className="flex flex-col gap-4">
-        <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+          ref={filePickerRef}
+          className="hidden"
+        />
+        <div
+          className="relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full"
+          onClick={() => filePickerRef.current.click()}
+        >
+          {imageFileUploadingProgress && (
+            <CircularProgressbar
+              value={imageFileUploadingProgress || 0}
+              text={`${imageFileUploadingProgress}%`}
+              strokeWidth={5}
+              styles={{
+                root: {
+                  width: "100%",
+                  height: "100%",
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                },
+                path: {
+                  stroke: `rgba(62,152,199,${
+                    imageFileUploadingProgress / 100
+                  })`,
+                },
+              }}
+            />
+          )}
           <img
-            src={currentUser.profilePicture}
+            src={imageFileUrl || currentUser.profilePicture}
             alt="user"
-            className="rounded-full w-full h-full object-cover border-8 border-[lightgray]"
+            className={`rounded-full w-full h-full object-cover border-8 border-[lightgray] ${
+              imageFileUploadingProgress &&
+              imageFileUploadingProgress < 100 &&
+              "opacity-60"
+            }`}
           />
         </div>
+        {imageFileUploadError && <Alert>{imageFileUploadError}</Alert>}
+
         <TextInput
           type="text"
           id="username"
--- clientmarnblog/src/components/DashSidebar.jsx
+++ clientmarnblog/src/components/DashSidebar.jsx
@@ -26,6 +26,7 @@ const DashSidebar = () => {
               icon={HiUser}
               label={"User"}
               labelColor="dark"
+              as="div"
             >
               Profile
             </Sidebar.Item>
